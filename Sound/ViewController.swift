652c619d7f0652c5cfd618ad419a8c978252b251
okada hatsune <swan.nilda1211@gmail.com>
2020-05-12 17:50:43 +0900

    　サウンドアプリ完成

---
+++ Sound/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by Hatsune Okada on 5/5/20.
+//  Copyright © 2020 Okada Hattsunn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet var drumButton: UIButton!
+    
+    @IBOutlet var pianoButton: UIButton!
+    
+     @IBOutlet var guitarButton: UIButton!
+    
+    let drumSoundPlayer=try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func touchDownDrumButton(){
+
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        
+     drumSoundPlayer.currentTime = 0
+    
+        drumSoundPlayer.play ()
+    }
+    
+    @IBAction func touchUpDrumButton() {
+    
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+        
+        
+    }
+    
+    @IBAction func touchDownPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+
+    
+    @IBAction func touchDownGuitarButton(){
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpGuitarButton(){
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
+    }
+
+}
+
